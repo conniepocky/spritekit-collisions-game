449fbbfd7b27bae59c7c276e2e8e928a8494d767
Connie <c.amer@icloud.com>
2024-08-10 18:19:33 +0100

    collisions

--- Push Ball/GameScene.swift
+++ Push Ball/GameScene.swift
@@ -8,28 +8,31 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
-    let player = SKShapeNode(circleOfRadius: 15)
-    let terrain = SKShapeNode(rectOf: CGSize(width: 5000, height: 30))
+    let player = SKShapeNode(circleOfRadius: 20)
+    let ground = SKShapeNode(rectOf: CGSize(width: 5000, height: 30))
     
     override func didMove(to view: SKView) {
         
+        physicsWorld.contactDelegate = self
+        
         player.fillColor = .red
-        player.physicsBody = SKPhysicsBody(circleOfRadius: 15)
+        player.physicsBody = SKPhysicsBody(circleOfRadius: 20)
         player.physicsBody?.affectedByGravity = true
         player.physicsBody?.isDynamic = true
         player.physicsBody?.mass = 1
         player.position = .init(x:0, y:500)
         addChild(player)
         
-        terrain.fillColor = .green
-        terrain.strokeColor = .brown
-        terrain.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 5000, height: 30))
-        terrain.physicsBody?.affectedByGravity = false
-        terrain.physicsBody?.isDynamic = false
-        terrain.position = .init(x:0, y:-frame.height/4)
-        addChild(terrain)
+        ground.fillColor = .green
+        ground.strokeColor = .brown
+        ground.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 5000, height: 30))
+        ground.physicsBody?.affectedByGravity = false
+        ground.physicsBody?.isDynamic = false
+        ground.name = "ground"
+        ground.position = .init(x:0, y:-frame.height/4)
+        addChild(ground)
     }
     
     
@@ -48,10 +51,25 @@ class GameScene: SKScene {
             marble.physicsBody = SKPhysicsBody(circleOfRadius: 15)
             marble.physicsBody?.affectedByGravity = true
             marble.physicsBody?.isDynamic = true
-            marble.physicsBody?.mass = 5
+            marble.physicsBody?.mass = 7
             marble.position = touch.location(in: self)
+            marble.name = "marble"
+            
+            marble.physicsBody!.contactTestBitMask = marble.physicsBody!.collisionBitMask
             
             self.addChild(marble)
+        }
+    }
+    
+    func collisionBetween(marble: SKNode, object: SKNode) {
+        print("boing")
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        if contact.bodyA.node?.name == "marble" && contact.bodyB.node?.name == "ground" {
+            collisionBetween(marble: contact.bodyA.node!, object: contact.bodyB.node!)
+        } else if contact.bodyA.node?.name == "ground" && contact.bodyB.node?.name == "marble" {
+            collisionBetween(marble: contact.bodyB.node!, object: contact.bodyA.node!)
         }
     }
     
